def44040ae6c61a863bc3d0a28126a511db1ee57
vikdenic <vik.denic@gmail.com>
2015-01-24 02:28:06 -0600

    Initial Commit

---
+++ CMP_Donate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CMP_Donate
+//
+//  Created by Vik Denic on 1/24/15.
+//  Copyright (c) 2015 mobilemakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
