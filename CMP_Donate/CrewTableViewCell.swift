b4ff7fba6ed41a5473f6a91ef45945aedd62e46a
vikdenic <vik.denic@gmail.com>
2015-02-06 23:53:47 -0600

    position crew imageview

--- CMP_Donate/CrewTableViewCell.swift
+++ CMP_Donate/CrewTableViewCell.swift
@@ -36,6 +36,9 @@ class CrewTableViewCell: UITableViewCell {
         super.layoutSubviews()
         let frame = self.contentView.bounds
         collectionView.frame = CGRectMake(0, 0.5, frame.size.width, frame.size.height - 1)
+        
+        let layout = collectionView.collectionViewLayout as UICollectionViewFlowLayout
+        layout.minimumLineSpacing = 0
     }
 
     func setCollectionViewDataSourceDelegate(dataSourceDelegate delegate: protocol<UICollectionViewDelegate,UICollectionViewDataSource>, index: NSInteger) {
